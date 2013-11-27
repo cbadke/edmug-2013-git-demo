71778d9262f6fd7af4d4b6267e8079cd0bdb02b7
Bob <bob@example.com>
2013-11-27 06:48:37 -0700

    Test if file exists and print filename

--- Show-PathInfo.ps1
+++ Show-PathInfo.ps1
@@ -2,4 +2,8 @@ param(
     [String] $FilePath
     )
 
-Write-Host $FilePath
+if (Test-Path $FilePath) {
+    Write-Host "Filename: $(Split-Path -Leaf $FilePath)"
+} else {
+    Write-Host "$FilePath not found!"
+}
