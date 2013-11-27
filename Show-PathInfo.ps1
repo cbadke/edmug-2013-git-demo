7457cace36d0c90e8efc46f32f0034f5136e3c49
Alice <alice@example.com>
2013-11-27 06:52:13 -0700

    Expand the path in-case the given path is relative

--- Show-PathInfo.ps1
+++ Show-PathInfo.ps1
@@ -3,6 +3,9 @@ param(
     )
 
 if (Test-Path $FilePath) {
+
+    $FilePath = Resolve-Path $FilePath
+
     Write-Host "Parent: $(Split-Path -Parent $FilePath)"
     Write-Host "Filename: $(Split-Path -Leaf $FilePath)"
 } else {
